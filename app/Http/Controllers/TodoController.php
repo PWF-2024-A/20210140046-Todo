1854e8f7f78f51188c82a379edc1aa1320df0a55
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2024-04-04 13:55:13 +0700

    create, read, search data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -12,9 +12,14 @@ class TodoController extends Controller
      */
     public function index()
     {
-        $todos = Todo::where('user_id', auth()->user()->id)->get();
-        dd($todos);
-        return view('todo.index');
+        $todos = Todo::where('user_id', auth()->user()->id)
+            ->orderBy('is_complete', 'asc')
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        // dd($todos);
+
+        return view('todo.index', compact('todos'));
     }
 
     /**
@@ -25,8 +30,52 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function edit()
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo created successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Todo $todo)
+    {
+        //nampilin satu2
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Todo $todo)
     {
         return view('todo.edit');
     }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Todo $todo)
+    {
+        //update sesuatu
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Todo $todo)
+    {
+        //hapus data
+}
 }
